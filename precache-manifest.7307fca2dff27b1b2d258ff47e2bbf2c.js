81b50bd6d600d1dc10549486f467810d4a534b77
Khach Tonoyan <tonoyan.khachik@gmail.com>
2020-07-08 15:42:09 +0400

    Updates

---
+++ precache-manifest.7307fca2dff27b1b2d258ff47e2bbf2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65eabcc0162fae240730d64b49b204dd",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "99a94811ab71c2ad51ed",
+    "url": "/WeatherForecast/static/css/main.05f9da77.chunk.css"
+  },
+  {
+    "revision": "c83bdb8916e9556bb41b",
+    "url": "/WeatherForecast/static/js/2.60de3060.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/WeatherForecast/static/js/2.60de3060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a94811ab71c2ad51ed",
+    "url": "/WeatherForecast/static/js/main.9f653a6c.chunk.js"
+  },
+  {
+    "revision": "6bcc6bed3cd4b200a86d",
+    "url": "/WeatherForecast/static/js/runtime-main.8d6656b5.js"
+  },
+  {
+    "revision": "59d6255841de103af75d40f572b73f8d",
+    "url": "/WeatherForecast/static/media/1.59d62558.jpg"
+  }
+]);
